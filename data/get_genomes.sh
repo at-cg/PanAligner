4980aa8291cb1587d72d3d87902bfac8edc6f1bf
jyotshna <jyotshnar@iisc.ac.in>
2023-01-31 01:44:39 -0800

    Minicycle data

--- data/get_genomes.sh
+++ data/get_genomes.sh
@@ -1,4 +1,4 @@
-conda activate minichain_data
+conda activate minicycle_data
 echo "Downloading Genomes..."
 curl -o HPRC-yr1.agc https://zenodo.org/record/5826274/files/HPRC-yr1.agc?download=1
 echo "Extracting Genomes..."
