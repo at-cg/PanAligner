43aeae6d82e26a9e8a1628b02ab14d631f337df6
JYOTSHNA RAJPUT <96770803+jyotshna-atcg@users.noreply.github.com>
2023-05-15 00:54:00 +0530

    Updated

--- data/graph_run.sh
+++ data/graph_run.sh
@@ -1,29 +1,20 @@
 echo "Mapping reads with minigraph..."
-./minigraph/minigraph -t32 -cx lr CHM13Y_95H.gfa Reads/CHM13_reads_5%.fa  > Results/minigraph/CHM13Y_95H_5%.gaf
-./minigraph/minigraph -t32 -cx lr CHM13Y_80H.gfa Reads/CHM13_reads_5%.fa  > Results/minigraph/CHM13Y_80H_5%.gaf
-./minigraph/minigraph -t32 -cx lr CHM13Y_40H.gfa Reads/CHM13_reads_5%.fa  > Results/minigraph/CHM13Y_40H_5%.gaf
-./minigraph/minigraph -t32 -cx lr CHM13Y_10H.gfa Reads/CHM13_reads_5%.fa  > Results/minigraph/CHM13Y_10H_5%.gaf
-echo "Mapping reads with minichain..."
-../minichain -t32 -cx lr CHM13Y_95H.gfa Reads/CHM13_reads_5%.fa > Results/minichain/CHM13Y_95H_5%.gaf
-../minichain -t32 -cx lr CHM13Y_80H.gfa Reads/CHM13_reads_5%.fa > Results/minichain/CHM13Y_80H_5%.gaf
-../minichain -t32 -cx lr CHM13Y_40H.gfa Reads/CHM13_reads_5%.fa > Results/minichain/CHM13Y_40H_5%.gaf
-../minichain -t32 -cx lr CHM13Y_10H.gfa Reads/CHM13_reads_5%.fa> Results/minichain/CHM13Y_10H_5%.gaf
+./minigraph/minigraph -t32 -cx lr CHM13Y_95H.gfa Reads/CHM13Y_5%_reads.fa  > Results/minigraph/CHM13Y_95H_5%.gaf
+./minigraph/minigraph -t32 -cx lr CHM13Y_80H.gfa Reads/CHM13Y_5%_reads.fa  > Results/minigraph/CHM13Y_80H_5%.gaf
+./minigraph/minigraph -t32 -cx lr CHM13Y_40H.gfa ReadsCHM13Y_5%_reads.fa  > Results/minigraph/CHM13Y_40H_5%.gaf
+./minigraph/minigraph -t32 -cx lr CHM13Y_10H.gfa Reads/CHM13Y_5%_reads.fa  > Results/minigraph/CHM13Y_10H_5%.gaf
+echo "Mapping reads with PanAligner..."
+../minichain -t32 -cx lr CHM13Y_95H.gfa Reads/CHM13Y_5%_reads.fa > Results/PanAligner/CHM13Y_95H_5%.gaf
+../minichain -t32 -cx lr CHM13Y_80H.gfa Reads/CHM13Y_5%_reads.fa > Results/PanAligner/CHM13Y_80H_5%.gaf
+../minichain -t32 -cx lr CHM13Y_40H.gfa Reads/CHM13Y_5%_reads.fa > Results/PanAligner/CHM13Y_40H_5%.gaf
+../minichain -t32 -cx lr CHM13Y_10H.gfa Reads/CHM13Y_5%_reads.fa > Results/PanAligner/CHM13Y_10H_5%.gaf
 echo "Mapping reads with GraphAligner..."
-/usr/bin/time -v ./bin/GraphAligner -t32 -x vg -g CHM13Y_95H.gfa -f Reads/CHM13_reads_5%.fa  -a Results/GraphAligner/CHM13Y_95H_5%.gaf
-/usr/bin/time -v ./bin/GraphAligner -t32 -x vg -g CHM13Y_80H.gfa -f Reads/CHM13_reads_5%.fa  -a Results/GraphAligner/CHM13Y_80H_5%.gaf
-/usr/bin/time -v ./bin/GraphAligner -t32 -x vg -g CHM13Y_40H.gfa -f Reads/CHM13_reads_5%.fa  -a Results/GraphAligner/CHM13Y_40H_5%.gaf
-/usr/bin/time -v ./bin/GraphAligner -t32 -x vg -g CHM13Y_10H.gfa -f Reads/CHM13_reads_5%.fa  -a Results/GraphAligner/CHM13Y_10H_5%.gaf
+/usr/bin/time -v ./bin/GraphAligner -t32 -x vg -g CHM13Y_95H.gfa -f Reads/CHM13Y_5%_reads.fa  -a Results/GraphAligner/CHM13Y_95H_5%.gaf
+/usr/bin/time -v ./bin/GraphAligner -t32 -x vg -g CHM13Y_80H.gfa -f Reads/CHM13Y_5%_reads.fa  -a Results/GraphAligner/CHM13Y_80H_5%.gaf
+/usr/bin/time -v ./bin/GraphAligner -t32 -x vg -g CHM13Y_40H.gfa -f Reads/CHM13Y_5%_reads.fa  -a Results/GraphAligner/CHM13Y_40H_5%.gaf
+/usr/bin/time -v ./bin/GraphAligner -t32 -x vg -g CHM13Y_10H.gfa -f Reads/CHM13Y_5%_reads.fa  -a Results/GraphAligner/CHM13Y_10H_5%.gaf
 ./k8 mgutils.js stableGaf CHM13Y_10H.gfa Results/GraphAligner/CHM13Y_10H_5%.gaf > Results/GraphAligner/CHM13Y_10H_5%_stable.gaf
 ./k8 mgutils.js stableGaf CHM13Y_40H.gfa Results/GraphAligner/CHM13Y_40H_5%.gaf > Results/GraphAligner/CHM13Y_40H_5%_stable.gaf
 ./k8 mgutils.js stableGaf CHM13Y_80H.gfa Results/GraphAligner/CHM13Y_80H_5%.gaf > Results/GraphAligner/CHM13Y_80H_5%_stable.gaf
 ./k8 mgutils.js stableGaf CHM13Y_95H.gfa Results/GraphAligner/CHM13Y_95H_5%.gaf > Results/GraphAligner/CHM13Y_95H_5%_stable.gaf
-echo "Mapping reads with GraphChainer..."
-/usr/bin/time -v ./bin/GraphChainer -t32 -g CHM13Y_95H.gfa -f Reads/CHM13_reads_5%.fa  -a Results/GraphChainer/CHM13Y_95H_5%.gaf
-/usr/bin/time -v ./bin/GraphChainer -t32 -g CHM13Y_80H.gfa -f Reads/CHM13_reads_5%.fa  -a Results/GraphChainer/CHM13Y_80H_5%.gaf
-/usr/bin/time -v ./bin/GraphChainer -t32 -g CHM13Y_40H.gfa -f Reads/CHM13_reads_5%.fa  -a Results/GraphChainer/CHM13Y_40H_5%.gaf
-/usr/bin/time -v ./bin/GraphChainer -t32 -g CHM13Y_10H.gfa -f Reads/CHM13_reads_5%.fa  -a Results/GraphChainer/CHM13Y_10H_5%.gaf
-./k8 mgutils.js stableGaf CHM13Y_10H.gfa Results/GraphChainer/CHM13Y_10H_5%.gaf > Results/GraphChainer/CHM13Y_10H_5%_stable.gaf
-./k8 mgutils.js stableGaf CHM13Y_40H.gfa Results/GraphChainer/CHM13Y_10H_5%.gaf > Results/GraphChainer/CHM13Y_40H_5%_stable.gaf
-./k8 mgutils.js stableGaf CHM13Y_80H.gfa Results/GraphChainer/CHM13Y_10H_5%.gaf > Results/GraphChainer/CHM13Y_80H_5%_stable.gaf
-./k8 mgutils.js stableGaf CHM13Y_95H.gfa Results/GraphChainer/CHM13Y_95H_5%.gaf > Results/GraphChainer/CHM13Y_95H_5%_stable.gaf
 echo "Mapped reads to graphs"
